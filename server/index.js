fff2dce068e82bdf9a841532047f57de980b34b9
Chukwualuka Chiama <lukaschiama@gmail.com>
2019-05-12 16:21:59 +0100

    initialize babel and eslint

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { PORT } = process.env;
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(morgan('dev'));
+
+app.listen(PORT, () => {
+  console.log(`We make magic on port ${PORT}`);
+})
