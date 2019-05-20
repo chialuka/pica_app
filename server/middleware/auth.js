5ed3d0299828117b35ef5d08a8c01ae3fcddd524
Chukwualuka Chiama <lukaschiama@gmail.com>
2019-05-20 14:58:02 +0100

    add social login

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,1 +1,68 @@
-import { Strategy } from 'passport-google-oauth20';
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import { Strategy as FacebookStrategy } from 'passport-facebook';
+import { ExtractJwt, Strategy as JwtStrategy } from 'passport-jwt';
+import dotenv from 'dotenv';
+import { Users } from '../models';
+
+dotenv.config();
+
+const {
+  JWT_SECRET,
+  GOOGLE_ID,
+  GOOGLE_SECRET,
+  FACEBOOK_ID,
+  FACEBOOK_SECRET,
+} = process.env;
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: JWT_SECRET,
+};
+
+const jwtStrategy = passport.use(
+  new JwtStrategy(jwtOptions, (payload, done) => {
+    console.log(payload, 'jwt payload');
+    Users.findOne({ where: { id: payload.sub } })
+      .then(user => done(null, user))
+      .catch(error => done(error, false));
+  }),
+);
+
+const googleStrategy = passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_ID,
+      clientSecret: GOOGLE_SECRET,
+      callbackURL: 'http://localhost:2800/signup/google',
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      console.log(profile, 'profile');
+      Users.findOne({ where: { googleId: profile.id } })
+        .then((user) => {
+          console.log(user);
+        })
+        .catch(err => console.log(err, 'err'));
+    },
+  ),
+);
+
+const facebookStrategy = passport.use(
+  new FacebookStrategy(
+    {
+      clientID: FACEBOOK_ID,
+      clientSecret: FACEBOOK_SECRET,
+      callbackURL: 'http://localhost:2800/signup/facebook',
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      console.log(profile, 'facebook profile');
+      Users.findOne({ where: { googleId: profile.id } })
+        .then((user) => {
+          console.log(user);
+        })
+        .catch(err => console.log(err, 'err'));
+    },
+  ),
+);
+
+export { jwtStrategy, googleStrategy, facebookStrategy };
