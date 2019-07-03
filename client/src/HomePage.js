088d69458e4f08235b6104cdfa1cb348f3b8227b
Chukwualuka Chiama <lukaschiama@gmail.com>
2019-07-03 18:27:57 +0100

    add background image

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Home = () => {
-    return <div>Welcome Home</div>
+    return <div>
+        <img className='image' src='people.jpg' alt='people connecting'/>
+    </div>
 };
 
 export default Home;
