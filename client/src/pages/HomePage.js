ee03a9572c1522311ba528f22da0087cfb977035
Chukwualuka Chiama <lukaschiama@gmail.com>
2019-07-07 23:56:06 +0100

    add google font-awesome

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 const Home = () => {
   return (
     <div className='home-back'>
       <img className='image' src='connect.jpg' alt='people connecting' />
       <FontAwesomeIcon icon={faFacebook} />
+      <FontAwesomeIcon icon={faGoogle} />
     </div>
   );
 };
