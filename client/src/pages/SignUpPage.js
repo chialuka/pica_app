0c08ce9a488d0f4b0e47a7d4add672dc714ab053
Chukwualuka Chiama <lukaschiama@gmail.com>
2019-07-10 19:24:22 +0100

    add signup form

---
+++ client/src/pages/SignUpPage.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { withRouter, Link } from "react-router-dom";
+import axios from 'axios';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+const SignUp = () => {
+  const [userDetails, setUserDetails] = useState({
+    fullName: '',
+    userName: '',
+    email: '',
+    password: ''
+  });
+
+  const handleChange = e => {
+    setUserDetails({
+      ...userDetails,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const createUser = async props => {
+    try {
+      const newUser = await axios.post('/users', userDetails);
+      if (newUser.status === 201) {
+        props.history.push('/dashboard')
+      }
+    } catch (error) {
+      return error;
+    }
+  }
+
+  const { fullName, userName, email, password } = userDetails;
+  return (
+    <Paper>
+      Let's get to know you!
+      <TextField
+        required
+        label="Full Name"
+        name="fullName"
+        value={fullName}
+        onChange={handleChange}
+      />
+      <TextField
+        required
+        label="Username"
+        name="userName"
+        value={userName}
+        onChange={handleChange}
+      />
+      <TextField
+        required
+        label="Email address"
+        name="email"
+        value={email}
+        onChange={handleChange}
+      />
+      <TextField
+        required
+        label="Password"
+        name="password"
+        value={password}
+        onChange={handleChange}
+      />
+      <Button variant="contained" onClick={createUser} style={{ margin: 15 }}>
+        Sign Up
+      </Button>
+      <div>Have an account?<Link to='/signin'>Login here</Link></div>
+    </Paper>
+  );
+};
+
+export default withRouter(SignUp);
